8809515b293d2e1bff512c96e10d1319ac4a48ee
kt2650380 <ktayl011@ucr.edu>
2017-07-24 23:08:05 -0700

    Signed-off-by: kt2650380 <ktayl011@ucr.edu>

---
+++ Homework/Savitch_9_7_PracProg3/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.cpp
+ * Author: Kyra Taylor
+ * Created on July 23, 2017, 07:48 PM
+ * Purpose:  Practice Program 1, Where is the 2 in an array.  
+ */
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <cmath>
+#include <string>
+using namespace std;
+
+void swapFrontBack(int in[], int len);
+int main()
+{
+  int arr1[5]={2,0,4,5,15}
+,arr2[9]={3,2,10,2,8,2,2,2,56};
+cout<<"Before swapping in arr1:\n first element="<
+        
+  }
+
+ 
+
+
